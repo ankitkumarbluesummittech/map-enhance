8d047b7d0528a882066f35d7c92b09a7b0a3f650
ankitkumarbluesummittech <150895491+ankitkumarbluesummittech@users.noreply.github.com>
2024-01-25 07:39:17 +0000

    low map app initalized

---
+++ lowmapapp/src/app/map/map.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Deck } from '@deck.gl/core/typed';
+// import { ScatterplotLayer } from '@deck.gl/layers/typed';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
